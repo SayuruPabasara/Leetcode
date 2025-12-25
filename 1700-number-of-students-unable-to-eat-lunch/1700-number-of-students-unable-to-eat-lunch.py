69446b419b0edd13439edd3017a9497399b92352
SayuruPabasara <sayurueragoda200254@gmail.com>
2025-12-25 23:13:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def countStudents(self, students, sandwiches):
+        count0=students.count(0)
+        count1=students.count(1)
+        for s in sandwiches:
+            if s==o:
+                if count0==0:
+                    break
+                else:
+                    count0-=1
+            else:
+                if count1==0:
+                    break
+                else:
+                    count1-=1
+        return count0+count1
+
+
+
+            
+        
